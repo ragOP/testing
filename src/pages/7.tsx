df71f412ef5071313fd91cc86ac34791bcd52557
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-05-02 21:46:39 +0530

    Update 7.tsx

--- src/pages/7.tsx
+++ src/pages/7.tsx
@@ -362,7 +362,7 @@ export default function Fifth_SP() {
 
               </div>
               <div className="main-des-5"  style={{marginTop:'-5px'}}>
-              If you have not yet claimed your monthly allowance then answer the questions below and once approved <b>you will have your $3,600 Grocery Allowance mailed to you within a few days ready for use!</b>
+              If you have not tested that your DNA is completely safe from these deadly diseases through a high-quality genetic screening, which is zero cost as well - get it done now while it’s still covered under Medicare.
               </div>
               {/* <div className='main-des-5' style = {{marginTop:"1rem"}}><b>Simplemente responda las siguientes preguntas:</b></div> */}
             </div>

5b7dc2806a8355626ab7ece652c96757668eaeb3
ragOP <najmi.raghib@gmail.com>
2024-05-14 22:07:59 +0530

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.scss';
 import NotFound from './pages/404';
 import S from './pages/7'
 import Simple from './pages/8'
+import Urgent from './pages/9'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
              <Route path = "/ft28" element = {<S />} />
              <Route path = "/ftyt28" element = {<Simple />} />
+             <Route path = "/ftb28" element = {<Urgent />} />
           <Route path = "/*" element = {<NotFound />} />
         </Routes>
       </div>

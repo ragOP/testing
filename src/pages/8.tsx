ec70ba62de5c2b09144d166a09c1fe2522bb1a73
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-05-16 16:18:07 +0530

    Update 8.tsx

--- src/pages/8.tsx
+++ src/pages/8.tsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast, cssTransition } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Head_bg from "../assets/body.png";
 import abba from "../assets/abba.png";
-import abc from "../assets/abc.jpeg";
+import abc from "../assets/res.png";
 import Headline from "../assets/headline_spandeb1.png";
 
 // google tag manager
